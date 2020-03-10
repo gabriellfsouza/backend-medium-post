e97b1a2b4914c848edfa20617ea1f279d65a4de8
Gabriel Lima <gabriellfsouza@gmail.com>
2020-03-10 06:28:42 -0300

    todo router validation middleware included

--- src/routes/todos.js
+++ src/routes/todos.js
@@ -1,18 +1,20 @@
 import { Router } from 'express';
 
+import StoreValidator from '../app/validators/ToDo/StoreValidator';
+import UpdateValidator from '../app/validators/ToDo/UpdateValidator';
 import ToDoController from '../app/controllers/ToDoController';
 
 const routes = new Router();
 
 routes
   .route('/')
-  .post(ToDoController.store)
+  .post([StoreValidator], ToDoController.store)
   .get(ToDoController.index);
 
 routes
   .route('/:_id')
   .get(ToDoController.show)
-  .put(ToDoController.update)
+  .put([UpdateValidator], ToDoController.update)
   .delete(ToDoController.destroy);
 
 export default routes;
